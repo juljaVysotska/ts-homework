5aca713601d6eb9bff10292fe28969b094443756
juliaEvseenko-zenchef <julia.evseenko@zenchef.com>
2025-11-05 14:58:47 +0200

    feat: lesson-4 - type issues

--- src/lesson-4/src/components/getTaskHtml.ts
+++ src/lesson-4/src/components/getTaskHtml.ts
@@ -1,18 +1,19 @@
+import type { Task } from '../types';
+
 export const getTaskHtml = ({
   title,
   description,
   deadline,
   status,
   priority,
-}: any) => `
-
+}: Task) => `
 <li class="bg-white shadow-md rounded-lg p-4">
 <h2 class="text-lg font-semibold">${title}</h2>
 <p class="text-gray-600 text-sm">${description}</p>
 <div class="flex justify-between text-sm mt-2">
     <span class="text-blue-600 font-medium ">Status: ${status}</span>
     <span class="text-yellow-600 font-medium">Priority: ${priority}</span>
-    <span class="text-gray-500">Deadline: ${deadline.split('T')[0]}</span>
+    <span class="text-gray-500">Deadline: ${deadline.toISOString().split('T')[0]}</span>
 </div>
 </li>
 
